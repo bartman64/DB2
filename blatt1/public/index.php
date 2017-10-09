00f6b26b371c3b98da48b44a67ba22be04b48577
SBenz <sbenz.96@gmx.de>
2017-10-09 18:30:15 +0200

    Updated index.php

--- blatt1/public/index.php
+++ blatt1/public/index.php
@@ -1,5 +1,9 @@
 <?php
+$conn = mysqli_connect("localhost", "root", "", "uebung1");
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
 
 <form action="" method="post">
