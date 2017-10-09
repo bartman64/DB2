d296978bfbe0d9d3b59310a45fc2b3fa727a3d97
sbenz <sbenz.96@gmx.de>
2017-10-09 21:54:42 +0200

    added fancy layout

--- blatt1/public/index.php
+++ blatt1/public/index.php
@@ -5,11 +5,20 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
-
-<form action="" method="post">
-    <select>
-    <option value="">Fc Bayern</option>
-    </select>
-    <br><br>
-    <input type="submit">
-</form>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../res/css/main.css">
+</head>
+<body>
+<div class="vote-field">
+    <h3 class="vote-field--header">Vereinvoting!</h3>
+    <form action="../res/php/updateVote.php" method="post">
+        <select>
+        </select>
+        <input type="text" name="newClub" value="">
+        <br><br>
+        <input type="submit">
+    </form>
+</div>
+</body>
+</html>
