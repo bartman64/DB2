12db94be84814b673baaac220fad0c4af7213edf
SBenz <sbenz.96@gmx.de>
2017-10-16 14:36:13 +0200

    Updated updateVote.php

--- blatt1/public/index.php
+++ blatt1/public/index.php
@@ -9,6 +9,7 @@ if (!$conn) {
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Cache-Control" content="no-store" />
     <link rel="stylesheet" type="text/css" href="../res/css/main.css">
 </head>
 <body>
--- blatt1/res/php/updateVote.php
+++ blatt1/res/php/updateVote.php
@@ -1,15 +1,73 @@
 <?php
-if($_POST['existingClubs'] != 'default' || $_POST['newClub'] != 'Neuer Verein') {
 
-    $selectedClub = $_POST['existingClubs'];
+$conn = mysqli_connect("localhost", "root", "", "uebung1");
 
-    $conn = mysqli_connect("localhost", "root", "", "uebung1");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+$existingClubs_default = 'default';
+$newClub_default = 'Neuer Verein';
+
+$existingClubs = mysqli_real_escape_string($conn, $_POST['existingClubs']);
+$newClub = mysqli_real_escape_string($conn, $_POST['newClub']);
+
+$queryResult;
+$querySuccess = false;
+$sumVotes;
+
+
+if ($existingClubs !== $existingClubs_default && $newClub === $newClub_default) {
+    $query = "UPDATE vereine SET Stimmen=Stimmen+1 WHERE `Name`= '" . $existingClubs . "'";
+
+    if (mysqli_query($conn, $query)) {
+        $queryResult = 'Abstimmung erfolgreich!';
+        $querySuccess = true;
+    } else {
+        $queryResult = 'Error updating record: ' . mysqli_error($conn);
+        mysqli_close($conn);
+    }
+
+} else if ($existingClubs === $existingClubs_default && $newClub !== $newClub_default) {
+    $noDuplicate = true;
+
+    //Get existing Vereine Namen
+    $query = "SELECT Name FROM vereine;";
+    mysqli_query($conn, "SET NAMES 'utf8'");
+    $result = mysqli_query($conn, $query);
+
+    if (!$result) {
+        $queryResult = 'Error getting records: ' . mysqli_error($conn);
+    };
+
+    while ($row = mysqli_fetch_row($result)) {
+        if (!$noDuplicate) {
+            break;
+        }
+        if (strcasecmp($row[0], $newClub) === 0) {
+            $noDuplicate = false;
+            $queryResult = 'Bitte keine Vereine doppelt eintragen!';
+            mysqli_close($conn);
+        }
+    }
+
+    if ($noDuplicate) {
+        mysqli_free_result($result);
+
+        $insertQuery = "INSERT INTO vereine (`Name`, Stimmen)
+        VALUES ('" . $newClub . "', 1)";
+
+        if (mysqli_query($conn, $insertQuery)) {
+            $queryResult = 'Record updated successfully';
+            $querySuccess = true;
+        } else {
+            $queryResult = 'Error updating record: ' . mysqli_error($conn);
+            mysqli_close($conn);
+        }
     }
 } else {
-    echo "Please select or add a club!";
+    $queryResult = 'Bitte wählen sie einen bereits bestehenden Verein aus oder tragen sie einen neuen ein!';
+    mysqli_close($conn);
 }
 ?>
 
@@ -19,22 +77,59 @@ if($_POST['existingClubs'] != 'default' || $_POST['newClub'] != 'Neuer Verein') 
     <link rel="stylesheet" type="text/css" href="../css/main.css">
 </head>
 <body>
-<div class="vote-field">
-    <h3 class="vote-field__header">Ergebnisse!</h3>
-    <h2>Gesamt Anzahl der Teilnehmer</h2>
-    <table>
-        <tr>
-            <th>Verein</th>
-            <th>Stimmen</th>
-            <th>Entfallene Stimmen pro Verein</th>
-        </tr>
-        <tr>
+<div>
+    <h2>
+        <?php
+        echo $queryResult;
+        ?>
+    </h2>
+    <?php
+    if ($querySuccess) {
+        $voteQuery = "SELECT SUM(Stimmen) as Participants FROM vereine";
 
-        </tr>
-        <tr>
+        $voteResult = mysqli_query($conn, $voteQuery);
 
-        </tr>
-    </table>
+        if (!$voteResult) {
+            echo 'Error updating record: ' . mysqli_error($conn);
+        } else {
+            $row = mysqli_fetch_row($voteResult);
+            $sumVotes = $row[0];
+            echo '<p>Anzahl der Teilnehmer: ' . $sumVotes . '</p>';
+        }
+        mysqli_free_result($voteResult);
+    }
+    ?>
+    <a href="../../public/index.php">Zurück zur Vereins Auswahl!</a>
+    <?php
+    if ($querySuccess) {
+        $tableQuery = "SELECT Name, Stimmen FROM vereine";
+
+        mysqli_query($conn, "SET NAMES 'utf8'");
+        $tableResult = mysqli_query($conn, $tableQuery);
+
+        if (!$tableResult) {
+            echo 'Error receiving record: ' . mysqli_errno($conn);
+        } else {
+            echo '<table border="1">';
+            echo '<tr>';
+            echo '<th>Verein</th>';
+            echo '<th>Abstimmungsteilnehmer</th>';
+            echo '<th>Prozentualer Anteil der entfallenen Stimmen</th>';
+            while($row = mysqli_fetch_row($tableResult)){
+                echo '<tr>';
+                for($i = 0; $i <  mysqli_num_fields($tableResult); $i++){
+                    echo "<td>$row[$i]</td>";
+                }
+                $percentage = number_format($row[1] / $sumVotes, 2, '.', ' ');
+                echo "<td>$percentage%</td>";
+                echo '</tr>';
+            }
+            echo '</table>';
+
+            mysqli_close($conn);
+        }
+    }
+    ?>
 </div>
 </body>
 </html>
