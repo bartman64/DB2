e80f739585986c40af1f61e7fd65ccb2eca7f9f5
Sebastian Horvath <se.bi1994@hotmail.de>
2017-10-16 17:45:01 +0200

    login fields are set

--- blatt1/TeilB/public/index.php
+++ blatt1/TeilB/public/index.php
@@ -1,3 +1,24 @@
 <?php
 
 ?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<form action="" method="post">
+    Name:<br>
+    <input type="text" name="name" value="Max" onfocus=" if (this.value === 'Max') this.value = '';"/>
+    <br>
+    Email:<br>
+    <input type="text" name="email" value="muster@email.com" onfocus=" if (this.value === 'muster@email.com') this.value = '';"/>
+    <br><br>
+    <input type="checkbox" name="VereinMitglied" value="Mitglied">
+    Bin Mitglied in einem Fußballverein
+    <br>
+    <br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
