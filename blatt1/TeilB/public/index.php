c230352b903d5c107dedcfc8b317cf4f9672ce09
Sebastian Horvath <se.bi1994@hotmail.de>
2017-10-22 14:20:32 +0200

    added css formating

--- blatt1/TeilB/public/index.php
+++ blatt1/TeilB/public/index.php
@@ -5,39 +5,46 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <title>Mailinglist Subscription</title>
+    <link rel="stylesheet" type="text/css" href="../res/css/main.css">
 </head>
 <body>
+<h1>Subscribe to football a newsletters</h1>
 <form action="../res/php/subscribe.php" method="post">
-    Name:<br>
-    <input type="text" name="name" value="" required placeholder="Enter your name"/>
-    <br>
-    Email:<br>
-    <input type="email" name="email" placeholder="Enter your email">
-    <br><br>
-    <input type="checkbox" name="VereinMitglied" value="Mitglied">
-    Bin Mitglied in einem Fußballverein
-    <br>
-    <br>
-    <input type="checkbox" name="mailinglist[]" value="1Liga">
-    1. Liga
-    <br>
-    <input type="checkbox" name="mailinglist[]" value="2Liga">
-    2. Liga
-    <br>
-    <input type="checkbox" name="mailinglist[]" value="3Liga">
-    3. Liga
-    <br>
-    <input type="checkbox" name="mailinglist[]" value="Regionalliga">
-    Regionalliga Bayern
-    <br>
-    <input type="checkbox" name="mailinglist[]" value="WM">
-    WM 2018
-    <br>
-    <input type="checkbox" name="mailinglist[]" value="Nationalmannschaft">
-    Deutsche Nationalmannschaft
-    <br>
-    <br>
-    <input type="submit" value="Subscribe">
+    <p class="p">
+        Name:
+        <input type="text" name="name" value="" required placeholder="Enter your name"/>
+        <br>
+        Email:
+        <input type="email" name="email" placeholder="Enter your email">
+        <br><br>
+        <input type="checkbox" name="VereinMitglied" value="Mitglied">
+        Bin Mitglied in einem Fußballverein
+        <br>
+        <br>
+    <div class="list-email">
+        <input type="checkbox" name="mailinglist[]" value="1Liga">
+        1. Liga
+        <br>
+        <input type="checkbox" name="mailinglist[]" value="2Liga">
+        2. Liga
+        <br>
+        <input type="checkbox" name="mailinglist[]" value="3Liga">
+        3. Liga
+        <br>
+        <input type="checkbox" name="mailinglist[]" value="Regionalliga">
+        Regionalliga Bayern
+        <br>
+        <input type="checkbox" name="mailinglist[]" value="WM">
+        WM 2018
+        <br>
+        <input type="checkbox" name="mailinglist[]" value="Nationalmannschaft">
+        Deutsche Nationalmannschaft
+    </div>
+    </p>
+    <div>
+        <input type="submit" value="Subscribe">
+    </div>
 </form>
 </body>
 </html>
